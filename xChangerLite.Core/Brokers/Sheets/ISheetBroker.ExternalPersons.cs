793c944d205f8b8a7eb56eb300ef1a41c9baa1ff
Ulugbek <ubakhromov22@gmail.com>
2023-02-10 01:13:16 +0500

    BROKERS: Select All External Person

---
+++ xChangerLite.Core/Brokers/Sheets/ISheetBroker.ExternalPersons.cs
@@ -0,0 +1,16 @@
+﻿//====================================================
+// Copyright (c) Coalition Of Good-Hearted Engineers
+// EVERY LITTLE HELPS
+//====================================================
+
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using xChangerLite.Core.Models.Foundations.ExternalPerson;
+
+namespace xChangerLite.Core.Brokers.Sheets
+{
+    public partial interface ISheetBroker
+    {
+        ValueTask<List<ExternalPerson>> SelectAllExternalPersonsAsync();
+    }
+}
---
+++ xChangerLite.Core/Models/Foundations/ExternalPersons/ExternalPerson.cs
@@ -0,0 +1,20 @@
+﻿//====================================================
+// Copyright (c) Coalition Of Good-Hearted Engineers
+// EVERY LITTLE HELPS
+//====================================================
+
+
+namespace xChangerLite.Core.Models.Foundations.ExternalPerson
+{
+    public class ExternalPerson
+    {
+        public string PersonName { get; set; }
+        public int Age { get; set; }
+        public string PetOne { get; set; }
+        public string PetOneType { get; set; }
+        public string PetTwo { get; set; }
+        public string PetTwoType { get; set; }
+        public string PetThree { get; set; }
+        public string PetThreeType { get; set; }
+    }
+}
