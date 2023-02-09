5df77ea18ff481592631ced8487f0b3928b1a606
Ulugbek <ubakhromov22@gmail.com>
2023-02-09 19:28:12 +0500

    INFRA: Initializing Unit Test Project

---
+++ xChangerLite.Core.Tests.Unit/DeleteMe.cs
@@ -0,0 +1,11 @@
+namespace xChangerLite.Core.Tests.Unit
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
--- xChangerLite.Core/Startup.cs
+++ xChangerLite.Core/Startup.cs
@@ -1,6 +1,43 @@
-﻿namespace xChangerLite.Core
+﻿//====================================================
+// Copyright (c) Coalition Of Good-Hearted Engineers
+// EVERY LITTLE HELPS
+//====================================================
+
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+
+namespace xChangerLite.Core
 {
     public class Startup
     {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+
+                app.UseSwaggerUI(config =>
+                    config.SwaggerEndpoint(
+                        url: "/swagger/v1/swagger.json",
+                        name: "xChanger.Core.POC v1"));
+            }
+
+            app.UseHttpsRedirection();
+            app.UseRouting();
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+                endpoints.MapControllers());
+        }
     }
 }
